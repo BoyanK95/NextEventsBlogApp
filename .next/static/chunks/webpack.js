0e761a72b369b89253175cfb4280bed095527fdb
BoyanK95 <boyan_k_95@abv.bg>
2023-04-16 23:40:10 +0300

    Created MainHeader in Layout component. Used them for navigation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1fa7eb06aafafd8a"; }
+/******/ 		__webpack_require__.h = function() { return "8187c6071335ccea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
