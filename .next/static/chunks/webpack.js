d27c5c58674c94847d4e92a8ecd9ae86ffa2b84d
BoyanK95 <boyan_k_95@abv.bg>
2023-04-16 15:29:12 +0300

    Created icons and added them to EventItem components

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a7490d20d8301360"; }
+/******/ 		__webpack_require__.h = function() { return "6b9e33f56123c911"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,6 +1,8 @@
-import Link from 'next/Link';
-import classes from './EventItem.module.css';
 import Button from '../ui/Button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import classes from './EventItem.module.css';
 
 function EventItem({ title, image, date, location, id }) {
     const readableDate = new Date(date).toLocaleDateString('en-US', {
@@ -20,13 +22,20 @@ function EventItem({ title, image, date, location, id }) {
                     <h2>{title}</h2>
                 </div>
                 <div className={classes.date}>
+                    <DateIcon />
                     <time>{readableDate}</time>
                 </div>
                 <div className={classes.address}>
+                    <AddressIcon />
                     <address>{formattedAddress}</address>
                 </div>
                 <div className={classes.actions}>
-                    <Button link={exploreLink}>Explore Event</Button>
+                    <Button link={exploreLink}>
+                        <span>Explore Event</span>
+                        <span className={classes.icon}>
+                            <ArrowRightIcon />
+                        </span>
+                    </Button>
                 </div>
             </div>
         </li>
