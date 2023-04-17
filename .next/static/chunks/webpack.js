facb9149de7fd34d68c36622a9d6a2bc28e36065
BoyanK95 <boyan_k_95@abv.bg>
2023-04-18 00:55:57 +0300

    Created FilteredEventsPage. Handled all age cases.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d4e8784ed9fcc4fb"; }
+/******/ 		__webpack_require__.h = function() { return "502b9ebbe2b37fcb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
